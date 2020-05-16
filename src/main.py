43cedffaedc4caf00f417dfa41125654c98d2427
Gerardo Alvarado <gerardoalvarado.bionico@gmail.com>
2020-05-16 11:20:54 -0500

    El mensaje secreto es christianvanderhenst

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
-# Resolve the problem!!
+# -*- config: utf-8 -*-
+
+import re
 
 
 def run():
-    # Start coding here
-
+    with open('encoded.txt', 'r', encoding='utf-8') as f:
+        code = f.read()
+    
+    pattern = re.compile(r'[a-z]')
+    decode = ''.join(pattern.findall(code))
+    print("El mensaje secreto es: ",decode)
 
 if __name__ == '__main__':
     run()
